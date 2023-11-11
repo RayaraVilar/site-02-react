ee2fa22254c94ef1f6a3a6d297b37383b79d31c9
Rayara <ravtra@hotmail.com>
2023-11-11 12:19:48 -0300

    first commit

---
+++ src/Componentes/Teste/Maria.js
@@ -0,0 +1,21 @@
+function Maria() {
+  return (
+    <div className='cabecalho'>
+      <div className="logo">
+        <img src="/imagens/logo.png" alt="Logo" />
+      </div>
+      <div className="navegacao">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Pages</a></li>
+          <li><a href="#">Testimonials</a></li>
+          <li><a href="#">Contact support</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Maria;
