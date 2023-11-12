ab50a6e08c635d23d6e8539bbda581d2bf8d8707
Rayara <ravtra@hotmail.com>
2023-11-11 21:43:00 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import './Componentes/Teste/Mariana.css';
+import './Componentes/Principal/Principal.css'
 import Maria from './Componentes/Teste/Maria';
+import Principal from './Componentes/Principal/Principal';
 
 function App() {
   return (
     <div className="App">
       <Maria/>
-   
+      <Principal/>
     </div>
   );
 }
---
+++ src/Componentes/Principal/Principal.js
@@ -0,0 +1,27 @@
+function Principal () {
+    return(
+        <div className="Main">
+            <div className="text">
+                <p>
+                    Get <span>ready</span> for your business & upgrade <span>all aspects</span>
+                </p>
+            </div>
+            <div className="paragrafo">
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus luctus eleifend nibh, vitae ultricies tellus porttitor in. Fusce massa elit, mollis eget sodales vel, ultrices ac arcu. Pellentesque eu blandit turpis, sit amet pharetra nulla. Quisque quis erat ut lacus pulvinar blandit et vel mi. Curabitur imperdiet lorem non neque dictum gravida. Nullam accumsan mauris vel odio laoreet, ullamcorper aliquet metus convallis. Suspendisse potenti. Sed cursus dolor eu diam luctus placerat. Aenean euismod quam at convallis aliquet. Aliquam blandit porttitor purus in luctus. Nunc quis ipsum mattis, pharetra lectus a, pharetra risus.
+                </p> 
+
+            </div>
+            <div className="botoes">
+                <button className="botao1">
+                    Discover More
+                </button>
+                <button className="botao2">
+                    Contact us
+                </button>
+            </div>
+        </div>
+        )
+}
+
+export default Principal
